794e061cb384beace24a0b1ba45ffa05bcf62504
alex.kovalevv <alex.kovalevv@gmail.com>
2018-01-11 12:46:10 +0300

    start

---
+++ controls/wp-editor.php
@@ -0,0 +1,80 @@
+<?php
+
+	/**
+	 * WP Editor Control
+	 *
+	 * Main options:
+	 *  name            => a name of the control
+	 *  value           => a value to show in the control
+	 *  default         => a default value of the control if the "value" option is not specified
+	 *  tinymce         => an array of options for tinymce
+	 * @link http://codex.wordpress.org/Function_Reference/wp_editor
+	 *
+	 * @author Paul Kashtanoff <paul@byonepress.com>
+	 * @copyright (c) 2013, OnePress Ltd
+	 *
+	 * @package factory-forms
+	 * @since 1.0.0
+	 */
+	class FactoryForms000_WpEditorControl extends FactoryForms000_Control {
+
+		public $type = 'wp-editor';
+
+		/**
+		 * Preparing html attributes and options for tinymce.
+		 *
+		 * @since 1.0.0
+		 * @return void
+		 */
+		protected function beforeHtml()
+		{
+			if( empty($this->options['tinymce']) ) {
+				$this->options['tinymce'] = array();
+			}
+			if( !isset($this->options['tinymce']['content_css']) ) {
+				$this->options['tinymce']['content_css'] = FACTORY_FORMS_000_URL . '/assets/css/editor.css';
+			}
+		}
+
+		/**
+		 * Shows the html markup of the control.
+		 *
+		 * @since 1.0.0
+		 * @return void
+		 */
+		public function html()
+		{
+			$nameOnForm = $this->getNameOnForm();
+			$value = $this->getValue();
+
+			?>
+			<div class='factory-form-wp-editor'>
+				<?php wp_editor($value, $nameOnForm, array(
+					'textarea_name' => $nameOnForm,
+					'wpautop' => false,
+					'teeny' => true,
+					'tinymce' => $this->getOption('tinymce', array())
+				)); ?>
+			</div>
+		<?php
+		}
+
+		/**
+		 * Returns a submit value of the control by a given name.
+		 *
+		 * @since 1.0.0
+		 * @return mixed
+		 */
+		public function getSubmitValue($name, $subName)
+		{
+			$nameOnForm = $this->getNameOnForm($name);
+			$value = isset($_POST[$nameOnForm])
+				? $_POST[$nameOnForm]
+				: null;
+			if( is_array($value) ) {
+				$value = implode(',', $value);
+			}
+
+			return wp_kses_post($value);
+		}
+	}
